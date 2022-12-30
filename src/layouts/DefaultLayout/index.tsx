97ada86b4ca2ba3e28d48b459498d7045d5f1775
Philipe Faria <philipefaria61@gmail.com>
2022-12-30 11:52:52 -0300

    style: Create Header Componente and implements navigation between pages

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from 'react-router-dom'
 
-import { Header } from '../components/Header'
+import { Header } from '../../components/Header'
+
+import { LayoutContainer } from './styles'
 
 function Defaultlayout() {
   return (
-    <div>
+    <LayoutContainer>
       <Header />
       <Outlet />
-    </div>
+    </LayoutContainer>
   )
 }
 
