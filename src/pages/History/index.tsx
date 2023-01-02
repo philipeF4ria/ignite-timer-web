83997b1c96df6aa123e4d2336e781d6b3970f775
Philipe Faria <philipefaria61@gmail.com>
2023-01-02 13:56:10 -0300

    fix: Fix date bug on history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -30,7 +30,7 @@ function History() {
                 <td>{cycle.task}</td>
                 <td>{cycle.minutesAmount} minutos</td>
                 <td>
-                  {formatDistanceToNow(cycle.startDate, {
+                  {formatDistanceToNow(new Date(cycle.startDate), {
                     addSuffix: true,
                     locale: ptBR,
                   })}
